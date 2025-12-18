69fa56a2885184ffc6cbfbae0988195387938f4d
Amidwar <amidwar05@gmail.com>
2025-12-17 21:33:27 -0500

    Corrigiendo error de .h

--- src/Persona.h
+++ src/Persona.h
@@ -1,1 +1,18 @@
-//Persona.h
+#ifndef PERSONA_H
+#define PERSONA_H
+
+#include<string>
+using namespace std;
+
+class Persona{
+	protected:
+		string nombre;
+		string dni;
+		int edad;
+	public:
+		Persona();
+		Persona(string nombre, string dni, int edad);
+		void mostrarPersona();
+};
+
+#endif
