da5a9828a1fc39dac2ca69b9cd7dabf858b667e8
Amidwar <amidwar05@gmail.com>
2025-12-17 22:10:03 -0500

    Implementando el constructor de cliente

--- src/Cliente.cpp
+++ src/Cliente.cpp
@@ -1,1 +1,9 @@
-//CLIENTE.CPP
+#include "Cliente.h"
+
+Cliente::Cliente(){
+}
+
+Cliente::Cliente(string nombre, string dni, int edad, int id, string codigo):Persona(nombre,dni,edad){
+	this->id=id;
+	this->correo=correo;
+}
