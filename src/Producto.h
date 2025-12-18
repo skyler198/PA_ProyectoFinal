be5a683b417b05547632312f021777ab735b149c
josue <josue.mlpro.2014@gmail.com>
2025-12-17 21:53:49 -0500

    creando la clase producto en producto.h

--- src/Producto.h
+++ src/Producto.h
@@ -1,1 +1,25 @@
 //Producto.h
+#ifndef PRODUCTO_H
+#define PRODUCTO_H
+
+#include <string>
+using namespace std;
+
+class Producto {
+private:
+    int codigo;
+    string nombreComida;
+    float precio;
+    int stock;
+    string descripcion;
+
+public:
+    Producto();
+    Producto(int codigo, string nombreComida, float precio, int stock, string descripcion);
+
+    float getPrecio();
+    string getNombre();
+    void mostrarProducto();
+};
+
+#endif
