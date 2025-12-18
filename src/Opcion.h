dc959f87b9bf6b31c5b4b7c755f793f310b5fcd2
maricielo <cielotapia198@gmail.com>
2025-12-17 21:22:17 -0500

    Declarando la clase Opcion en Opcion.h

--- src/Opcion.h
+++ src/Opcion.h
@@ -1,1 +1,21 @@
 //opcion.h
+#ifndef OPCION_H
+#define OPCION_H
+
+#include<string>
+using namespace std;
+
+class Opcion{
+private:
+    string nombre;
+    float precioExtra;
+
+public:
+    Opcion();
+    Opcion(string nombre, float precioExtra);
+
+    string getNombre();
+    float getPrecioExtra();
+};
+
+#endif
