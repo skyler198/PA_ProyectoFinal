2a15a128333dba0304f01652fb117e59b62e1754
maricielo <cielotapia198@gmail.com>
2025-12-17 21:33:04 -0500

    Implementando las funciones de Opcion.h en Opcion.cpp

--- src/Opcion.cpp
+++ src/Opcion.cpp
@@ -1,1 +1,18 @@
 //Opcion.cpp
+#include "Opcion.h"
+
+Opcion::Opcion(){
+}
+
+Opcion::Opcion(string n, float Pe){
+    nombre = n;
+    precioExtra = Pe;
+}
+
+string Opcion::getNombre() {
+    return nombre;
+}
+
+float Opcion::getPrecioExtra() {
+    return precioExtra;
+}
