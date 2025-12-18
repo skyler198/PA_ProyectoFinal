a369be470a3842bdf23890aeaa814dec7ecd9cc9
maricielo <cielotapia198@gmail.com>
2025-12-17 22:14:42 -0500

    Implementando las funciones de PedidoPersonalizado.h en PedidoPersonalizado.cpp

--- src/PedidoPersonalizado.cpp
+++ src/PedidoPersonalizado.cpp
@@ -1,1 +1,28 @@
 //PedidoPersonalizado.cpp
+#include "PedidoPersonalizado.h"
+#include <iostream>
+using namespace std;
+
+PedidoPersonalizado::PedidoPersonalizado(){
+    cantOpciones = 0;
+}
+
+PedidoPersonalizado::PedidoPersonalizado(Producto producto){
+    this->producto = producto;
+    cantOpciones = 0;
+}
+
+void PedidoPersonalizado::agregarOpcion(Opcion opcion){
+    opciones[cantOpciones++] = opcion;
+}
+
+float PedidoPersonalizado::calcularPrecioFinal(){
+    float total = producto.getPrecio();
+    for (int i = 0; i < cantOpciones; i++)
+        total += opciones[i].getPrecioExtra();
+    return total;
+}
+
+void PedidoPersonalizado::mostrarDetalle(){
+    cout << producto.getNombre() << "S/ " << calcularPrecioFinal() << endl;
+}
