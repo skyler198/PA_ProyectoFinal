4dcb70618b0bfbb2de3e13b1ccf0d285ec10f204
josue <josue.mlpro.2014@gmail.com>
2025-12-17 21:36:20 -0500

    declarando la clase vendedor en vendedor.h

--- src/Vendedor.h
+++ src/Vendedor.h
@@ -1,1 +1,20 @@
 //Vendedor.h
+#ifndef VENDEDOR_H
+#define VENDEDOR_H
+
+#include "Persona.h"
+
+class Vendedor : public Persona {
+	private:
+    	string usuario;
+    	string contrasena;
+		float sueldo;
+	public:
+    	Vendedor();
+    	Vendedor(string nombre, string dni, int edad,string usuario, string contrasena);
+		void emitirFactura();
+		void recepcionarPedido();
+		void getRol();
+};
+
+#endif
