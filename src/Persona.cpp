392dbf30ca47bfa174075ff76a9b0469cbf0f08f
maricielo <cielotapia198@gmail.com>
2025-12-18 09:41:37 -0500

    Mejora de la funcion mostarPersona y imlementacion de getDNI

--- src/Persona.cpp
+++ src/Persona.cpp
@@ -1,16 +1,23 @@
 #include "Persona.h"
-#include<iostream>
+#include <iostream>
+
 using namespace std;
 
-Persona::Persona(){
-}
+Persona::Persona(){}
 
 Persona::Persona(string nombre, string dni, int edad){
-	this->nombre=nombre;
-	this->dni=dni;
-	this->edad=edad;
+    this->nombre = nombre;
+    this->dni = dni;
+    this->edad = edad;
 }
 
 void Persona::mostrarPersona(){
-	cout<<nombre<<"-"<<dni<<"-"<<edad<<endl;
+    cout << "Nombre: " << nombre << endl;
+    cout << "DNI:    " << dni << endl;
+    cout << "Edad:   " << edad << endl;
 }
+
+string Persona::getDNI(){
+    return dni;
+}
+
